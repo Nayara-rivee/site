3e58c9b6dbf31385fe0dd096ba37e0d050553e98
RiveeMoon <nayarasp07@gmail.com>
2025-06-04 06:40:31 -0300

    alteações form sites dos planos

--- sitebasico.php
+++ sitebasico.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
-    <link rel="stylesheet" href="./css/sitesdinamico.css">
+    <link rel="stylesheet" href="./css/Sitesdinamico.css">
     <link href="https://cdn.jsdelivr.net/gh/codeOpacity/op_icons@main/op_icons.all.min.css" rel="stylesheet" />
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
@@ -113,52 +113,63 @@
         </div>
     </div>
 
-    <div class="contato">
-        <img src="./img/orcamento.svg" alt="" />
-        <h1>Faça suas preferëncias</h1>
-        <form name="form1" id="form1" method="POST" action="#">
-            <div class="name">
-                <label for="name">Nome</label>
-                <input
-                    type="text"
-                    name="firstname"
-                    id="firstname"
-                    placeholder="Digite seu primeiro nome"
-                    required />
-            </div>
-            <div class="email">
-                <label for="email">Email</label>
-                <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    placeholder="Digite seu email"
-                    required />
-            </div>
-            <div class="tell">
-                <label for="number">WhatsApp</label>
-                <input
-                    type="tel"
-                    name="number"
-                    id="number"
-                    placeholder="(xx) xxxx-xxxx"
-                    required />
-            </div>
-            <div class="mensagem">
-                <textarea name="mensagem" id="mensagem" placeholder="Mensagem"></textarea>
-            </div>
-            <div class="button-form">
-                <button><a href="index.php">Fazer orçamento</a></button>
-            </div>
 
-        </form>
-        <div class="meios-contato">
-            <h4>Nossos meios de comunicação</h4>
-            <p>@instagramempresa</p>
-            <p>(xx) xxxxx-xxxx</p>
-            <p>facebook</p>
+        <!-- Contact Form Section (Overlapping) -->
+
+    <div class="container form-container-overlap">
+        <div class="row justify-content-center" data-aos="fade-up" data-aos-delay="300">
+            <div class="col-lg-10">
+                <div class="contact-form-wrapper">
+                    <h2 class="text-center mb-4">Get in Touch</h2>
+
+                    <form action="forms/contact.php" method="post" class="php-email-form">
+                        <div class="row g-3">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <div class="input-with-icon">
+                                        <i class="bi bi-person"></i>
+                                        <input type="text" class="form-control" name="name" placeholder="First Name" required="">
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <div class="input-with-icon">
+                                        <i class="bi bi-envelope"></i>
+                                        <input type="email" class="form-control" name="email" placeholder="Email Address" required="">
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <div class="input-with-icon">
+                                        <i class="bi bi-text-left"></i>
+                                        <input type="text" class="form-control" name="sbject" placeholder="Subject" required="">
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-12">
+                                <div class="form-group">
+                                    <div class="input-with-icon">
+                                        <i class="bi bi-chat-dots message-icon"></i>
+                                        <textarea class="form-control" name="message" placeholder="Write Message..." style="height: 180px" required=""></textarea>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12 text-center">
+                                <button type="submit" class="btn btn-primary btn-submit">SEND MESSAGE</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
+
     </div>
+
     <script src="./js/sitedinamico.js"></script>
 </body>
 
